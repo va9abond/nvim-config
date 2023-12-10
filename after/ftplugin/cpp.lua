d2d8fd86bf0659e3b5d9f627d0552c32229d7f1c
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-12-10 08:52:04 +0300

    chore: remove unused files

--- after/ftplugin/cpp.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_command('source ~/.config/nvim/lua/run_cpp.vim')
--- init.lua
+++ init.lua
@@ -2,6 +2,3 @@ require('core/base')
 require('core/binds')
 require('core/lazy')
 require('core/scripts')
-
--- vim.api.nvim_command('source ~/.config/nvim/lua/run_cpp.vim')
--- vim.api.nvim_command('source ~/.config/nvim/lua/core/presentation_mode.vim')
