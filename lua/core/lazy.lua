744102c316f9a83c6662297c299f5a6c1c33bcf7
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-12-11 18:44:45 +0300

    chore: repair colorscheme

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -174,7 +174,10 @@ local plugins = {
     {
         'metalelf0/jellybeans-nvim',
         lazy = false, priority = 1000,
-        dependencies = { 'rktjmp/lush.nvim' }
+        dependencies = { 'rktjmp/lush.nvim' },
+        config = function()
+            vim.cmd([[colorscheme jellybeans-nvim]])
+        end,
     },
 
 -- ---------------------------------------------------------
