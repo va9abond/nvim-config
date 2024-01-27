2d6af6cd75a16ed7e4683534df7b27e6e6eeb4c6
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-01-27 23:24:05 +0300

    wip: replace require to dofile for core

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
-require("core/base")
-require("core/mappings")
+-- local XDG_CONFIG_HOME_NVIM = "~/.config/nvim/"
+
+
+dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/opts.lua")
+dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/binds.lua")
+dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/misc/hl_on_yank.lua")
+
 vim.cmd("source lua/core/binds.vim")
-require("misc/hl_on_yank")
-require("core/lazy")
+
+-- require("lazy")
