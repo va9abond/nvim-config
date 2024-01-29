4ca3fba9062f9e77779f31b11b696780f2b2bfa6
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-01-29 17:30:04 +0300

    wip: transfer netrw binds in after/ftplugin

--- init.lua
+++ init.lua
@@ -6,6 +6,5 @@ dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/binds.lua")
 dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/modules_binds.lua")
 dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/misc/hl_on_yank.lua")
 
-vim.cmd("source lua/core/binds.vim")
 
 require("core/lazy")
