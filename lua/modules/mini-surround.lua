01cb5ed9ab28a826c492acf77d200b144e171352
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-01-29 19:55:14 +0300

    wip: get back old binds for surrounding
    'y', 'd', 'c' wait motion by default

--- lua/modules/mini-surround.lua
+++ lua/modules/mini-surround.lua
@@ -4,12 +4,12 @@ return {
     opts = {
         highlight_duration = 2000,
         mappings = {
-            add = 'sa', -- Add surrounding in Normal and Visual modes
-            delete = 'sr', -- Delete surrounding
+            add = 'ys', -- Add surrounding in Normal and Visual modes
+            delete = 'ds', -- Delete surrounding
             find = '', -- Find surrounding (to the right)
             find_left = '', -- Find surrounding (to the left)
             highlight = '', -- Highlight surrounding
-            replace = 'sc', -- Replace surrounding
+            replace = 'cs', -- Replace surrounding
             update_n_lines = '', -- Update `n_lines`
 
             suffix_last = '', -- Suffix to search with "prev" method
