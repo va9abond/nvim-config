84a5bf0b126d681da332a85b77d97ea99bef2526
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-01-29 19:14:34 +0300

    wip: add langmap_keys

--- lua/core/opts.lua
+++ lua/core/opts.lua
@@ -97,6 +97,14 @@ vim.g.spellfile_URL = "http://ftp.vim.org/vim/runtime/spell"
 vim.o.spelllang = "en,ru"
 vim.o.spelloptions = "camel"
 
+local langmap_keys = {
+  'ёЁ;`~', '№;#',
+  'йЙ;qQ', 'цЦ;wW', 'уУ;eE', 'кК;rR', 'еЕ;tT', 'нН;yY', 'гГ;uU', 'шШ;iI', 'щЩ;oO', 'зЗ;pP', 'хХ;[{', 'ъЪ;]}',
+  'фФ;aA', 'ыЫ;sS', 'вВ;dD', 'аА;fF', 'пП;gG', 'рР;hH', 'оО;jJ', 'лЛ;kK', 'дД;lL', [[жЖ;\;:]], [[эЭ;'\"]],
+  'яЯ;zZ', 'чЧ;xX', 'сС;cC', 'мМ;vV', 'иИ;bB', 'тТ;nN', 'ьЬ;mM', [[бБ;\,<]], 'юЮ;.>',
+}
+vim.opt.langmap = table.concat(langmap_keys, ',')
+
 
 ------------------------------------------------------------
                                                 -- XkbSwitch
