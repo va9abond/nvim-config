37a50a01ebe0d0649138b6f1021f0ac440b0c5b7
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-03-14 23:34:05 +0300

    wip: add setup for markdown

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,5 @@
+vim.bo.textwidth = 70
+-- ---------------------------------------------------------
+                                              -- Spell check
+vim.opt.spelllang = 'en_us,ru_ru'
+vim.opt.spell = true
