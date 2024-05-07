a6ed36fdedb470430c6e4eebd1ca2035ac44ad22
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-05-07 20:36:44 +0300

    create a nvim-cheat-sheet

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- local XDG_CONFIG_HOME_NVIM = "~/.config/nvim/"
 
 dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/base.lua")
-dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/binds.lua")
+dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/keymaps.lua")
 
 require("lazy-init")
