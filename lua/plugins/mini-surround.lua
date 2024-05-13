48381f522c5d7e74ee2d40d06e9281d4179fc21e
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-05-13 15:16:03 +0300

    plugins: replace mini-surround with nvim-surround

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -1,18 +1,18 @@
 return {
-    "echasnovski/mini.surround", cond = true, lazy = true,
-    event = "VeryLazy",
+    "echasnovski/mini.surround", cond = false,
+    lazy = true, event = "VeryLazy",
     version = false,
 
     opts = {
         highlight_duration = 2000,
         mappings = {
             -- TODO: change mapping for add surround to not conflict with yank
-            add = 'sa', -- Add surrounding in Normal and Visual modes
-            delete = 'sd', -- Delete surrounding
+            add = 'ys', -- Add surrounding in Normal and Visual modes
+            delete = 'ds', -- Delete surrounding
             find = '', -- Find surrounding (to the right)
             find_left = '', -- Find surrounding (to the left)
             highlight = '', -- Highlight surrounding
-            replace = 'sc', -- Replace surrounding
+            replace = 'cs', -- Replace surrounding
             update_n_lines = '', -- Update `n_lines`
 
             suffix_last = '', -- Suffix to search with "prev" method
@@ -20,3 +20,7 @@ return {
         },
     }
 }
+
+-- The problem is that I can't manage mappings in different modes specially
+-- in visual mode. When I want a copy in visual mode and press 'y' there is
+-- a delay - 1) I really want to yank or 2) I want surroundings
---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,27 @@
+return {
+    "kylechui/nvim-surround", cond = true,
+    lazy = true, event = "VeryLazy",
+    version = "*",
+    opts = { -- h: nvim-surround.txt
+        keymaps = {
+            insert = false,
+            insert_line = false,
+            normal = "ys",
+            normal_cur = "yss",
+            normal_line = false,
+            normal_cur_line = false,
+            visual = "S",
+            visual_line = false,
+            delete = "ds",
+            change = "cs",
+            change_line = false,
+        },
+
+        -- keymaps        -- Defines plugin keymaps
+        -- surrounds      -- Defines surround keys and behavior
+        -- aliases        -- Defines aliases
+        -- highlight      -- Defines highlight behavior
+        -- move_cursor    -- Defines cursor behavior
+        -- indent_lines   -- Defines line indentation behavior
+    }
+}
