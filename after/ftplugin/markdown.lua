c18ba319e68179d78571e444a3eff6378c431f68
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-05-15 21:14:13 +0300

    after: replace vim.opt by .opt_local in ftplugins

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
-vim.bo.textwidth = 70
+vim.opt_local.textwidth = 70
 -- vim.opt.formatoptions = 'rqnl1jt'
-vim.opt.formatoptions:append('t')
+vim.opt_local.formatoptions:append('t')
 -- ---------------------------------------------------------
                                               -- Spell check
-vim.opt.spelllang = 'en_us,ru_ru'
-vim.opt.spell = true
+vim.opt_local.spelllang = 'en_us,ru_ru'
+vim.opt_local.spell = true
