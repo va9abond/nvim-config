222618cba512f5d69dfc5ae887b86b556a3ea133
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-18 12:15:08 +0300

    chore: rename keymaps -> maps

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- local XDG_CONFIG_HOME_NVIM = "~/.config/nvim/"
 
 dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/settings.lua")
-dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/keymaps.lua")
+dofile(vim.env.XDG_CONFIG_HOME .. "/nvim/lua/core/maps.lua")
 
 require("lazy-init")
