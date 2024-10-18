868a3dd5eedd822ffa3a261a1de87cb3f29beb02
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-18 15:27:27 +0300

    colors: remove unused

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,9 @@
 return {
-    {
-        dir = "~/gitlocal/colors.nvim", cond = true,
-        lazy = false, priority = 1000,
-        dependencies = { "rktjmp/lush.nvim" },
+    dir = "~/gitlocal/colors.nvim", cond = true,
+    lazy = false, priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
 
-        config = function()
-            vim.cmd.colorscheme("mycolors")
-        end,
-    },
-    {
-        "blazkowolf/gruber-darker.nvim", cond = true,
-        lazy = true, --[[ priority = 1000, ]]
-        name = "blazkowolf-gruber-darker",
-        config = function()
-            require("gruber-darker")
-        end
-    },
+    config = function()
+        vim.cmd.colorscheme("mycolors")
+    end,
 }
