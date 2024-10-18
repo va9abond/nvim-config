cb97edda3cd4e614132fb824e82c2605569f77e3
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-18 12:11:59 +0300

    wip: tidy up

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,71 +6,55 @@ end
 vim.g.mapleader = ';'
 vim.g.maplocalleader = ';'
 -- ---------------------------------------------------------
-map({'n', 'v'}, "<Space>", "<Nop>")
+-- Other
+map('n', "<C-\\>", "<C-l>")
 map('n', "<C-f>", "<Nop>")
-map('i', "<LeftMouse>", "<Nop>")
-map('i', "<RightMouse>", "<Nop>")
--- Insert mode binds
+map({'n', 'v'}, "<Space>", "<Nop>")
+
+-- ---------------------------------------------------------
+-- Insert map-mode
 map('i', "jk", "<esc>")
 map('i', "<C-g>", "<del>")
 map('i', "<C-h>", "<BS>")
 map('i', "<BS>", "<NOP>")
-
-map('n', "<C-h>", "<C-w>h")
-map('n', "<C-l>", "<C-w>l")
-map('n', "<C-j>", "<C-w>j")
-map('n', "<C-k>", "<C-w>k")
+map('i', "<LeftMouse>", "<Nop>")
+map('i', "<RightMouse>", "<Nop>")
 
 
 -- ---------------------------------------------------------
-                                  -- Command-line mode binds
+-- Command-line map-mode
 map('c', "<C-b>", "<Left>")
 map('c', "<C-f>", "<Right>")
 -- map('c', "<C-j>", "<S-Left>")
 -- map('c', "<C-k>", "<S-Right>")
-
 map('c', "<C-g>", "<del>")
--- map('c', "<C-x><C-o>", "<del>")
-
--- map('c', '<C-p>', '<Up>')
--- map('c', '<C-n>', '<Down>')
 
 
 -- ---------------------------------------------------------
-                                                  -- Buffers
+-- Buffers
 map('n', "[b", "<cmd>bprev<cr>")
 map('n', "]b", "<cmd>bnext<cr>")
 map('n', "<leader>bl", "<cmd>ls<cr>") -- buffer list
+
 -- enable mini-bufremove
 -- map('n', "<leader>bd", "<cmd>bd!<cr>") -- check mini-bufremove (to save layout)
 -- map('n', "<leader>x", "<cmd>bd<cr>") -- buffer delete + close window
 
 
-map('n', "<leader>tn", "<cmd>tabnew<cr>") -- tab new
-map('n', "<leader>tc", "<cmd>tabclose<cr>") -- tab close
--- map('n', "<leader>tl", "<cmd>ls<cr>") -- tab list
-
-
 -- ---------------------------------------------------------
-                                               -- Fast quite
--- map('n', "<leader>q", "<cmd>q<cr>")
-
-
--- ---------------------------------------------------------
-                           -- Inserts blank line below/above
-map('n', "<CR>", "o<ESC>")
-map('n', "<S-CR>", "O<ESC>")
-
-
--- ---------------------------------------------------------
-                                             -- Split window
+-- Windows (:h window)
 map('n', "<C-w>v", "<CMD>vsplit<CR>")
--- map('n', "<C-w>x", "<CMD>split<CR>") -- <C-w>s originally
+map('n', "<C-w>z", "<C-w>|")
+
+map('n', "<C-h>", "<C-w><C-h>")
+map('n', "<C-j>", "<C-w><C-j>")
+map('n', "<C-k>", "<C-w><C-k>")
+map('n', "<C-l>", "<C-w><C-l>")
 
 -- <C-w>n - create new window with empty file as (:new)
 -- <C-w>q - similar to :q
 -- <C-w>o - make the current window the only one on the screen. All other
-         -- windows are closed
+--          windows are closed
 -- <C-w>w - next-layout window
 -- <C-w>W - prev-layout window
 -- <C-w>t - top-left window
@@ -78,47 +62,50 @@ map('n', "<C-w>v", "<CMD>vsplit<CR>")
 -- <C-w>p - last accessed window
 -- <C-w>P - go to preview window
 
-
--- ---------------------------------------------------------
-                                            -- Zooming panes
-map('n', "<C-w>z", "<C-w>|")
-
-
--- ---------------------------------------------------------
-                                             -- Panes resize
 map('n', "<C-Left>", "<cmd>vertical resize -5<cr>")
 map('n', "<C-Right>", "<cmd>vertical resize +5<cr>")
-map('n', "<C-Up>", "<cmd>res -5<cr>")
-map('n', "<C-Down>", "<cmd>res +5<cr>")
+map('n', "<C-Up>", "<cmd>resize -5<cr>")
+map('n', "<C-Down>", "<cmd>resize +5<cr>")
 
 
 -- ---------------------------------------------------------
-                                         -- Panes navigation
-map('n', "<C-h>", "<C-w><C-h>")
-map('n', "<C-j>", "<C-w><C-j>")
-map('n', "<C-k>", "<C-w><C-k>")
-map('n', "<C-l>", "<C-w><C-l>")
+-- Inserts blank line below/above
+map('n', "<CR>", "o<ESC>")
+map('n', "<S-CR>", "O<ESC>")
 
 
 -- ---------------------------------------------------------
--- Use operator pending mode to visually select the whole
--- buffer e.g. dA = delete buffer ALL,
---             yA = copy whole buffer
+-- Use operator pending mode to visually select the whole buffer
+-- dA = delete buffer ALL,
+-- yA = yank buffer ALL.
 map('o', 'A', ":<C-U>normal! mzggVG<CR>`z")
 map('x', 'A', ":<C-U>normal! ggVG<CR>")
 
+
 -- ---------------------------------------------------------
+-- Yank, cut, paste (system clipboard)
 map({'n', 'v'}, "<leader>y", [["+y]])
 map({'n', 'v'}, "<leader>p", [["+p]])
 map({'n', 'v'}, "<leader>d", [["+x]]) -- cut (yank in register and delete)
+
 map('n', "<leader>Y", [["+Y]])
--- map('n', "<leader>D", [["+D]])
 map('n', "<leader>P", [["+P]])
+
+map({'n', 'v'}, 'x', [["_x]])
 
 
 -- ---------------------------------------------------------
-                       -- Where do black hole register lead?
-map({'n', 'v'}, 'x', '"_x')
+-- Change text without putting it into the vim register
+-- see https://stackoverflow.com/q/54255/6064933
+map({'n', 'v'}, 'c', [["_c]])
+map({'n', 'v'}, 'C', [["_C]])
+
+
+-- ---------------------------------------------------------
+-- Replace visual selection with text in register,
+-- but not clean the register after
+-- see https://stackoverflow.com/q/10723700/6064933
+map('x', 'p', [["_c<Esc>p]])
 
 
 -- ---------------------------------------------------------
@@ -127,63 +114,48 @@ vim.keymap.set({'n', 'v'}, 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, sile
 
 
 -- ---------------------------------------------------------
-                             -- Fix * (Keep cursor position,
-                                -- don't move to next match)
+-- Fix * (keep cursor position, don't move to next match)
 map('n', '*', "*N")
 map('n', 'n', "nzzzv")
 map('n', 'N', "Nzzzv")
 
 
 -- ---------------------------------------------------------
-                            -- Page scrolling with centering
+-- Page scrolling with centering
 map('n', "<C-d>", "<C-d>zz")
 map('n', "<C-u>", "<C-u>zz")
--- map('n', "zH", "z<cr>")
--- map('n', "zM", "z.")
--- map('n', "zL", [[z-]])
--- map('n', "zH", "H")
--- map('n', "zM", "M")
--- map('n', "zL", "L")
 
 
 -- ---------------------------------------------------------
-                                   -- Move lines up and down
+-- Moves lines up and down
 map('v', 'J', ":m '>+1<CR>gv=gv")
 map('v', 'K', ":m '<-2<CR>gv=gv")
 
 
 -- ---------------------------------------------------------
-                                            -- Text aligning
+-- Text aligning
 map('n', "<leader>>", "<cmd>.right textwidth<cr>")
 map('n', "<leader><", "<cmd>.left 0<cr>")
 
 
 -- ---------------------------------------------------------
-                                                    -- Netrw
--- map('n', "<leader><leader>", vim.cmd.Lexplore)
--- map('n', '-', vim.cmd.Sexplore)
-
-
-                                 -- other binds in binds.vim
--- ---------------------------------------------------------
--- Do not include white space characters when using $ in visual mode;
-           -- see https://vi.stackexchange.com/q/12607/15292
-                        -- Go to start or end of line easier
-map({ 'n', 'v', 'o' }, 'H', '^')
-map({ 'n', 'v', 'o' }, 'L', 'g_')
+-- Go to start/end of line easier
+-- see https://vi.stackexchange.com/q/12607/15292
+map({'n', 'v', 'o'}, 'H', '^')
+map({'n', 'v', 'o'}, 'L', 'g_')
 
 
 -- ---------------------------------------------------------
-  -- Continuous visual shifting (does not exit Visual mode),
-             -- `gv` means to reselect previous visual area;
-                -- see https://superuser.com/q/310417/736190
+-- Continuous visual shifting (does not exit Visual mode),
+-- `gv` means to reselect previous visual area
+-- see https://superuser.com/q/310417/736190
 map('x', '<', '<gv')
 map('x', '>', '>gv')
 
 
 -- ---------------------------------------------------------
-             -- Reselect the text that has just been pasted;
-         -- see https://stackoverflow.com/a/4317090/6064933
+-- Reselect the text that has just been pasted
+-- see https://stackoverflow.com/a/4317090/6064933
 vim.keymap.set(
     'n', "<leader>v", "printf('`[%s`]', getregtype()[0])",
     { expr = true, desc = "Reselect last pasted area" }
@@ -191,28 +163,10 @@ vim.keymap.set(
 
 
 -- ---------------------------------------------------------
-    -- Change text without putting it into the vim register;
-            -- see https://stackoverflow.com/q/54255/6064933
-map({'n', 'v'}, 'c', [["_c]])
-map({'n', 'v'}, 'C', [["_C]])
-
-
--- ---------------------------------------------------------
-          -- Replace visual selection with text in register,
-                           -- but not clogging the register;
-         -- see https://stackoverflow.com/q/10723700/6064933
-map('x', 'p', [["_c<Esc>p]])
-
-
--- ---------------------------------------------------------
-map('n', "<C-\\>", "<C-l>")
-
-
--- ---------------------------------------------------------
--- Reference:
+-- Refs:
     -- echasnovski
-    -- ThePrimeagen,
-    -- jdhao,
+    -- ThePrimeagen
+    -- jdhao
     -- Tj DeVries
-    -- asbjornHaland,
+    -- asbjornHaland
     -- sdaschner
