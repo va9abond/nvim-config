a6a9a612e48ce9fc939e5a8a2eb66a597fd7db44
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-18 14:43:55 +0300

    wip: tidy up

--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -1,21 +1,20 @@
+-- ---------------------------------------------------------
 local set = vim.opt_local
-
+-- ---------------------------------------------------------
 -- Set local settings for terminal buffers
 vim.api.nvim_create_autocmd("TermOpen", {
     group = vim.api.nvim_create_augroup("custom-term-open", {}),
     callback = function()
         set.number = false
         set.relativenumber = false
+        set.cursorline = false
         set.scrolloff = 0
     end,
 })
 
--- vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+-- ---------------------------------------------------------
 vim.keymap.set('t', "<C-[>", "<C-\\><C-n>")
--- vim.keymap.set('t', "<C-h>", "<C-\\><C-w>h")
--- vim.keymap.set('t', "<C-l>", "<C-\\><C-w>l")
--- vim.keymap.set('t', "<C-j>", "<C-\\><C-w>j")
--- vim.keymap.set('t', "<C-k>", "<C-\\><C-w>k")
 
 -- Open a terminal at the bottom of the screen with a fixed height.
 vim.keymap.set("n", "<leader>tt", function()
