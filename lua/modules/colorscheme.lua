54210847acf33edd513e38c60f5f102bc6934ec0
rustem sirazetdinov <avesirazetdinov@gmail.com>
2025-02-08 14:56:22 +0300

    wip(colors): remove unused colorschemes

--- lua/modules/colorscheme.lua
+++ lua/modules/colorscheme.lua
@@ -1,27 +1,9 @@
 return {
-    {
-        dir = "~/gitlocal/x-gruber-darker.nvim", cond = true,
-        lazy = false, priority = 1000,
-        dependencies = { "rktjmp/lush.nvim" },
+    dir = "~/gitlocal/x-gruber-darker.nvim", cond = true,
+    lazy = false, priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
 
-        config = function()
-            vim.cmd.colorscheme("xgruber")
-        end,
-    },
-
-    {
-        "habamax/vim-habamax", lazy = false, priority = 1000,
-    },
-
-    {
-        "blazkowolf/gruber-darker.nvim",
-
-        -- config = function()
-        --     vim.cmd.colorscheme("gruber-darker")
-        -- end,
-    },
-
-    {
-        "metalelf0/jellybeans-nvim"
-    },
+    config = function()
+        vim.cmd.colorscheme("xgruber")
+    end,
 }
