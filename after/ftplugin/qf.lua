3b6bb45d5e860e0be1c073cb701f8cbe79af9f23
rustem sirazetdinov <avesirazetdinov@gmail.com>
2025-02-11 01:36:14 +0300

    ftplugin(qf): add setup for quickfix list

---
+++ after/ftplugin/qf.lua
@@ -0,0 +1,11 @@
+local opts = { noremap = true, silent = false, buffer = 0 }
+
+vim.keymap.set('n', '<C-p>', '<cmd>cprev<cr>', opts)
+vim.keymap.set('n', '<C-n>', '<cmd>cnext<cr>', opts)
+vim.keymap.set('n', 'P', '<cmd>cfirst<cr>', opts)
+vim.keymap.set('n', 'N', '<cmd>clast<cr>', opts)
+vim.keymap.set('n', '[Q', '<cmd>cold<cr>', opts)
+vim.keymap.set('n', ']Q', '<cmd>cnewer<cr>', opts)
+
+vim.opt_local.number = true
+vim.opt_local.relativenumber = false
